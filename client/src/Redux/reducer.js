c1205335e9f3124b706553d5387ce7a428b57529
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-03 16:00:18 -0300

    reducer done
    hay que chequear las actions que van en este front o en el admin.

--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -10,7 +10,8 @@ import {
 let initialState = {
     sermons: [],
     detail: [],
-    filters: []
+    filters: [],
+    coincidences: true,
 }
 
 function rootReducer(state = initialState, {type, payload}){
@@ -18,10 +19,38 @@ function rootReducer(state = initialState, {type, payload}){
     switch (type) {
 
         case GET_ALL_SERMON:
-          return{
-            ...state,
-            sermons: payload
-          }
+            return{
+                ...state,
+                sermons: payload
+            }
+            //fijarse si van en este o el otro front
+        case POST_SERMON:
+            return{
+                ...state,
+            }
+             //fijarse si van en este o el otro front
+        case UPDATE_SERMON:
+            return{
+                ...state
+            }
+             //fijarse si van en este o el otro front
+        case DELETE_SERMON:
+            return{
+                ...state
+            }
+        case SEARCH_BY_NAME:
+           if(payload.length > 0){
+            return{
+                ...state,
+                filters:payload,
+                coincidences: true
+            }
+           }else{
+            return{
+                ...state,
+                coincidences: false
+            }
+            }            
 
         default:
             return{
