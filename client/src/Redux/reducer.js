80e2556568a98d2ced1fd0496ad18ffe3675c0c4
Elizabeth MIcaela Gurdzel <121364740+elizabethMica@users.noreply.github.com>
2023-11-03 15:41:08 -0300

    reducer started, getAll

--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -1,3 +1,11 @@
+import { 
+    GET_ALL_SERMON,
+    GET_DETAIL,
+    UPDATE_SERMON,
+    POST_SERMON,
+    DELETE_SERMON,
+    SEARCH_BY_NAME
+ } from "./actionTypes";
 
 let initialState = {
     sermons: [],
@@ -8,6 +16,13 @@ let initialState = {
 function rootReducer(state = initialState, {type, payload}){
      
     switch (type) {
+
+        case GET_ALL_SERMON:{
+          return{
+            ...state,
+            sermons: payload
+          }
+        }
         default:
             return{
                 ...state
