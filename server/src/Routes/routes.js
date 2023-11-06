ec0dec26f3f69a495da8d4362b3c1417b2e70915
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-06 10:57:57 -0300

    contact routes updated

--- server/src/Routes/routes.js
+++ server/src/Routes/routes.js
@@ -10,16 +10,25 @@ const route = Router();
 //get all sermons
 route.get("/sermon", getSermonHandler );//funciona, get all y get by name
 
+//get all contact info
+route.get("/contact", )
+
 //get sermon detail
 route.get("/sermon/:id", getByIdHandler ); //funciona by id
 
 //post new sermon
 route.post("/sermon-post", postSermon); //funciona el post
 
-// //delete sermon by id
-route.delete("/sermon-delete/:id", deleteSermonHandler);
+//post contact info
+route.post("/contact-post", )
 
-// //update sermon by id
-route.patch("/sermon-update/:id", updateSermonHandler);
+//delete sermon by id
+route.delete("/sermon-delete/:id", deleteSermonHandler); //funciona el delete
+
+//delete contact by id
+route.delete("/contact-delete", )
+
+//update sermon by id
+route.patch("/sermon-update/:id", updateSermonHandler); //funciona el update (si o si hay que pasarle un title por ahora)
 
 module.exports = route;
