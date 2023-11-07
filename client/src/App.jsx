49e1765d1bd5bb0cc263a197dbc90372cdfc824f
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-07 17:46:44 -0300

    navbar added to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import {Routes, Route, useLocation} from "react-router-dom";
 import SermonHome from "./Components/SermonsHome/SermonHome";
 import SermonDetail from "./Components/Detail/SermonDetail";
 import Landing from "./Components/Landing/Landing";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
  
 
   return (
     <>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Landing/>}/>
         <Route path="/sermones" element={<SermonHome/>}/>
