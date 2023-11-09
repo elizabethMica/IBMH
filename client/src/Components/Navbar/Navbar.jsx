45326077eff61030d9f928d4b000ce591914a52d
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-09 13:26:25 -0300

    nav done

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import bookLogo from '/book.svg'
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Navigation = () => {
   return (
