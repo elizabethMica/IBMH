0a343dc5c57c3191847b77a5b6a9775a0e3538cb
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-08 21:13:53 -0300

    component contact

---
+++ client/src/Components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div><h1>acá poner la informacion de contacto, como llegar y formulario para que llenen con un mensaje y su info que luego va al admin</h1></div>
+  )
+}
+
+export default Contact
