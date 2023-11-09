ea5a5d8c070e24a074fd932b7489cf00b4489486
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-08 21:44:18 -0300

    logo en navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import bookLogo from '../../../public/book.svg'
 
 const Navbar = () => {
   return (
     <div>
-      <NavLink to="/" style={{textDecoration:"none"}}> Logo </NavLink>
+      <NavLink to="/" style={{textDecoration:"none"}}> <img style={{ whidth:30, height:30}}src={bookLogo} alt="Logo" /> </NavLink>
       <NavLink to="/sermones" style={{textDecoration:"none"}}> Sermones </NavLink>
       <NavLink to="/about" style={{textDecoration:"none"}}> Nosotros </NavLink>
       <NavLink to="/contact" style={{textDecoration:"none"}}> Contacto </NavLink>
