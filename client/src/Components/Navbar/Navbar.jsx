7ff8c7caa056efe62a88664b7b33ae40a37384ec
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-16 22:36:02 -0300

    nav responsive imported things

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import bookLogo from '/logoibm.png'
+import {AiOutlineMenu} from 'react-icons/ai'
+import {FaXmark} from 'react-icons/fa6'
 
 const Navigation = () => {
+
+  let Links =[
+    {name: 'Sermones', link: '/sermones'},
+    {name: 'Nosotros', link: '/about'},
+    {name: 'Contacto', link: '/contact'},
+  ]
+
+  const [isOpen, setIsOpen] = useState(false)
+  
   return (
     <>
     <nav>
