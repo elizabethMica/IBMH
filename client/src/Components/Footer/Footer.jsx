7b66f4048173366025879e848cd27cca04c4a0f8
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-16 23:02:00 -0300

    footer styling

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -16,62 +16,59 @@ function Footer() {
 
   return (
     <footer>
-    <div>
-      <div>
-      <div>
-      <NavLink to='/sermones'><img src={logoWhite} 
+    
+      <div className='bg-red-500'>
+
+
+          <div className='bg-green-500'>
+            <NavLink to='/sermones'><img src={logoWhite} 
               width="70"
               height="70"
               alt="IBM Hurlingham"/>
-         </NavLink>
-        <div>
-          <h3 >Iglesia Bautista Misionera Hurlingham</h3>
-          <p>
-            texto...
-          </p>
-        </div>
-      </div>
+            </NavLink>
+            <div className='bg-pink-500'>
+              <h3 >Iglesia Bautista Misionera Hurlingham</h3>
+              <p>texto...</p>
+            </div>
+          </div>
 
 
-      <div>
-        <div>
+        <div className='bg-white'>
           <h4>Contacto</h4> {/* navlink a contacto */}
-          <div>
-           <div>
-              <BiChurch color={'white'} size={20}/> Reunión general: <strong>Domingos 10:30hs</strong>
-           </div>
-           <div>
-              <FaLocationDot color={'white'} size={20} /> Schumann 2334, Hurlingham, Provincia de Buenos Aires
-           </div>
-           <div>
-              <MdEmail color={'white'} size={20} /> ibmhurlingham@gmail.com
-           </div>
+          <div className='bg-pink-500'>
+              <div>
+                <BiChurch color={'white'} size={20}/> Reunión general: <strong>Domingos 10:30hs</strong>
+              </div>
+              <div>
+                <FaLocationDot color={'white'} size={20} /> Schumann 2334, Hurlingham, Provincia de Buenos Aires
+              </div>
+              <div>
+                <MdEmail color={'white'} size={20} /> ibmhurlingham@gmail.com
+              </div>
           </div>
         </div>
-      </div>
+      
 
 
-      <div>
-        <div>
+     
+        <div className='bg-gray-500'>
           <h4>Redes Sociales</h4>
-          <div>
-          <div as={Link} to={youtubeLink} >
-              <FaYoutube color={'white'} size={20}/> IBM Hurlingham iglesia bautista     
-           </div>
-           <br/>
-           <div as={Link} to={spotifyLink} >
-              <BsSpotify color={'white'} size={20} /> IBM Hurlingham
-           </div>
-           <br/>
-           <div as={Link} to={facebookLink} >
-              <FaFacebookSquare color={'white'} size={20} /> Iglesia Bautista Misionera Hurlingham
-           </div>
+          <div className='bg-pink-500'>
+              <div as={Link} to={youtubeLink} >
+                <FaYoutube color={'white'} size={20}/> IBM Hurlingham iglesia bautista     
+              </div>
+              <div as={Link} to={spotifyLink} >
+                <BsSpotify color={'white'} size={20} /> IBM Hurlingham
+              </div>
+              <div as={Link} to={facebookLink} >
+                <FaFacebookSquare color={'white'} size={20} /> Iglesia Bautista Misionera Hurlingham
+              </div>
           </div>
         </div>
+
       </div>
-      </div>
-    </div>
-    </footer>
+      
+  </footer>
   )
 }
 
