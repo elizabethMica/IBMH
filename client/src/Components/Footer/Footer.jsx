adf0ba619d94427451e3add611d7f268e29ece2b
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-17 11:50:01 -0300

    footer responsive done

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -16,9 +16,9 @@ function Footer() {
 
   return (
     <footer>
-      <div className='bg-[#443426] flex flex-col justify-evenly py-6 md:flex-row md:items-start'>
+      <div className='bg-[#443426] flex flex-col justify-evenly p-6 md:flex-row md:items-start'>
             <div className='flex justify-center items-center md:items-start'>
-              <NavLink to='/sermones'><img src={logoWhite} 
+              <NavLink to='/'><img src={logoWhite} 
                 width="80"
                 height="80"
                 alt="IBM Hurlingham"/>
@@ -26,13 +26,14 @@ function Footer() {
             </div>
         <div className='bg-[#443426] flex flex-col justify-center items-center text-center px-4 md:w-72 md:text-start'>
             <div className='bg-[#443426]'>
-              <h3 className='text-lg font-bold text-white'>Iglesia Bautista Misionera Hurlingham</h3>
-              <p className='text-white text-center md:text-start px-10 md:px-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ex debitis, sunt doloribus dolore iste natus laborum id, hic, ullam odit minima numquam quod recusandae tempore ipsum quis architecto provident?</p>
+              <h3 className='text-lg  text-white'>Iglesia Bautista Misionera</h3>
+              <p className='text-white text-center md:text-start px-10 md:px-0 pt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ex debitis, sunt doloribus dolore iste natus laborum id, hic, ullam odit minima numquam quod recusandae tempore ipsum quis architecto provident?</p>
             </div>
+            <h5 className='text-sm  text-gray-400 md:py-4  py-2'>Conoce más acerca de la iglesia <NavLink to="/about" className={"hover:text-[#bd8351]"}>AQUÍ</NavLink></h5>
           </div>
 
-        <div className='bg-[#443426] flex  flex-col justify-center text-center my-4 md:text-start'>
-          <h3 className='text-lg font-bold text-white md:pb-4  pb-2'><NavLink to="/contact">Contacto</NavLink></h3> {/* navlink a contacto */}
+        <div className='bg-[#443426] flex  flex-col justify-center md:items-start text-center my-4 md:my-0 md:text-start'>
+          <h3 className='text-lg  text-white md:pb-4  pb-2'><NavLink to="/contact">Contacto</NavLink></h3> {/* navlink a contacto */}
           <div className='bg-[#443426] flex flex-col mx-4 text-white md:items-start md:m-0'>
               <div className=' flex flex-row justify-center md:pb-4 text-start pb-2'>
                 <BiChurch color={'white'} size={20} className='mx-2'/> Reunión general: Domingo 10:30hs
@@ -44,11 +45,11 @@ function Footer() {
                 <MdEmail color={'white'} size={20} className='mx-2'/> ibmhurlingham@gmail.com
               </div>
           </div>
-          <h3 className='text-lg font-bold text-white md:py-4  py-2'>Conoce más acerca de la iglesia <NavLink to="/about" className={"hover:text-[#bd8351]"}>AQUÍ</NavLink></h3>
+          
         </div>
       
-        <div className='bg-[#443426] flex  flex-col justify-center text-center my-4 md:text-start'>
-          <h3 className='text-lg font-bold text-white md:pb-4 pb-2'>Redes Sociales</h3>
+        <div className='bg-[#443426] flex  flex-col justify-center text-center my-4 md:my-0 md:text-start'>
+          <h3 className='text-lg  text-white md:pb-4 pb-2'>Redes Sociales</h3>
           <div className='bg-[#443426] flex flex-col mx-4 text-white justify-center items-center text-center md:items-start md:m-0'>
               <div className='flex flex-row justify-center md:pb-4 text-start pb-2'>
                 <FaYoutube color={'white'} size={20} className='mx-2'/>
@@ -58,11 +59,12 @@ function Footer() {
                 <BsSpotify color={'white'} size={20} className='mx-2' /> 
                 <NavLink to={spotifyLink} target="_blank">IBM Hurlingham</NavLink>
               </div>
-              <div className='flex flex-row justify-center  text-start' >
+              <div className='flex flex-row justify-center  text-start pb-2' >
                 <FaFacebookSquare color={'white'} size={20} className='mx-2'/> 
                 <NavLink to={facebookLink} target="_blank">Iglesia Bautista Misionera Hurlingham </NavLink>
               </div>
           </div>
+          <NavLink to="/contact" className={"lg:mt-16 mt-4 lg:self-end self-center"}><button className='text-lg bg-blue-700 rounded  text-white p-2'>Contáctanos</button></NavLink>
         </div>
 
       </div>
