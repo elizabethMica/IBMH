17b9c6fb08a0125e0f397274127110acc26a126a
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-21 18:05:52 -0300

    detail mobile view

--- client/src/Components/Detail/SermonDetail.jsx
+++ client/src/Components/Detail/SermonDetail.jsx
@@ -1,7 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import { useEffect } from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { getAllSermon, getDetail, clearDetail } from "../../Redux/actions";
 import SermonCard from "../Sermon/sermonCard";
 import LastFourSermonCard from "../Sermon/LastFourSermonCard";
@@ -15,32 +15,31 @@ const SermonDetail = () => {
     },[dispatch])
     
     const detail = useSelector(state => state.detail)
-
-    console.log(detail)
   
     const embed = "https://www.youtube.com/embed/"
     const videoLink = detail?.videoYT?.split("/")[3]
     
   return (
-    <>
-    <div >
-          <h3>{detail?.title}</h3>
-          <h5>{detail?.verse}</h5>
-      <section >
+    <div className="pt-20" >
+      <div className="">
+        <h3 className="text-lg font-bold text-center">{detail?.title}</h3>
+        <h5 className="text-sm text-center mb-2 w-full">{detail?.verse}</h5>
+      </div>
+          
+      <section className="mx-8">
         <div >
             {detail?.videoYT? (
                     <iframe
-                      key={detail?.id}
-                      src={embed + videoLink}
-                      width={1040}
-                      height={560}
-                    ></iframe>
-                ) : <p>Tu dispositivo no soporta la renderización. Por favor visita el canal de Youtube. IBM Hurlingham</p>         
+                    key={detail?.id}
+                    src={embed + videoLink }
+                    className="w-80 h-48 m-auto"
+                  />    
+                ) : <p>El video no es compatible con el celular. Por favor visita el canal de Youtube <Link target="_blank" to="https://www.youtube.com/@ibmhurlingham879" style={{color:"blue"}}>IBM Hurlingham</Link></p> 
             }
         </div>
-        <div >
-        <p >Descripción</p>
-        <p >{detail?.description}</p>
+        <div className="mb-10 mt-4 border-t-2 ">
+        <p className="mt-2 font-semibold text-sm">Descripción</p>
+        <p className="mt-2 text-justify">{detail?.description}</p>
         </div>
       </section>
 
@@ -53,9 +52,11 @@ const SermonDetail = () => {
           </div>
         </section>
     </div>
-      
-    </>
   )
 }
 
 export default SermonDetail
+
+
+
+
