de60ebbe690a25e3b13e68936d1c7937e59aae78
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-22 14:25:53 -0300

    action type get last four

--- client/src/Redux/actionTypes.js
+++ client/src/Redux/actionTypes.js
@@ -7,4 +7,5 @@ export const DELETE_SERMON = "DELETE_SERMON";
 export const POST_CONTACT = "POST_CONTACT";
 export const GET_ALL_CONTACT = "GET_ALL_CONTACT";
 export const DELETE_CONTACT = "DELETE_CONTACT";
-export const CLEAR_DETAIL = "CLEAR_DETAIL"
+export const CLEAR_DETAIL = "CLEAR_DETAIL";
+export const GET_LAST_FOUR = "GET_LAST_FOUR";
