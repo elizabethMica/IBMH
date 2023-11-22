007ace7d111cbdfe086cb8e9af872f5c2214062e
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-22 16:40:58 -0300

    form contact responsive done

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -3,8 +3,8 @@ import FormContact from './FormContact'
 
 function Contact() {
   return (
-    <div className='mx-8 py-20'>
-      <h1>acá poner la informacion de contacto, como llegar y formulario para que llenen con un mensaje y su info que luego va al admin</h1>
+    <div className='md:mx-8 py-20'>
+      <h1 className='mx-8'>acá poner la informacion de contacto, como llegar y formulario para que llenen con un mensaje y su info que luego va al admin</h1>
 
       <FormContact/>
     </div>
--- client/src/Components/Contact/FormContact.jsx
+++ client/src/Components/Contact/FormContact.jsx
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
+import {useDispatch} from 'react-redux'
+import {postContact} from '../../Redux/actions'
 
 function FormContact() {
+
+  const dispatch = useDispatch()
 
   const [value, setValue] = useState({
     name: "",
@@ -9,62 +13,74 @@ function FormContact() {
     phone: "",
     message: ""
   })
-
+  
   const handleChange =(event)=>{
-  event.preventDefaul()
+  event.preventDefault();
    setValue({
     ...value,
     [event.target.name] : event.target.value
    })
   }
 
+  const handleSubmit =(event)=>{
+    event.preventDefault();
+    dispatch(postContact(value))
+    alert("Mensaje enviado con exito. En breve nos contactaremos con usted!")
+  }
 
   return (
-    <form>
-       <div className='bg-red-500'>
-        <label> Nombre </label>
+    <form onSubmit={(event)=>handleSubmit(event)} className='bg-[#be8552] md:rounded-lg my-8 py-4 md:py-6 justify-center items-center flex  flex-col gap-2 md:gap-4 md:w-3/6 w-full'>
+       <div className=' w-[300px] md:w-full md:px-6 flex flex-col mx-8'>
+        <label className='text-sm text-white md:text-lg'> Nombre </label>
             <input 
+            className='w-full h-[45px] border-2  rounded-md text-start'
             type="text" 
-            placeholder="Ej: Liliana"
+            placeholder="ej: Liliana"
             name="name"
             onChange={handleChange}/>
        </div>
 
-       <div className='bg-red-500'>
-        <label> Apellido </label>
+       <div className=' w-[300px] md:w-full md:px-6 flex flex-col'>
+        <label className='text-sm text-white md:text-lg'> Apellido </label>
             <input 
+            className='w-full border-2 h-[45px] rounded-md text-start'
             type="text" 
-            placeholder="Ej: Ibañez"
+            placeholder="ej: Ibañez"
             name="lastName"
             onChange={handleChange}/>
        </div>
 
-       <div className='bg-red-500'>
-        <label> Email </label>
+       <div className=' w-[300px] md:w-full md:px-6 flex flex-col'>
+        <label className='text-sm text-white md:text-lg'> Email </label>
             <input 
+            className='w-full  border-2 h-[45px] rounded-md text-start'
             type="email" 
-            placeholder="liliana@gmail.com"
+            placeholder="ej: liliana@gmail.com"
             name="email"
             onChange={handleChange}/>
        </div>
 
-       <div className='bg-red-500'>
-        <label> Telefono </label>
+       <div className=' w-[300px] md:w-full md:px-6 flex flex-col'>
+        <label className='text-sm text-white md:text-lg'> Telefono </label>
             <input 
+            className='w-full  border-2 h-[45px] rounded-md text-start'
             type="tel" 
             placeholder="+54 1156789209"
             name="phone"
             onChange={handleChange}/>
        </div>
 
-       <div className='bg-red-500'>
-        <label> Mensaje </label>
+       <div className=' w-[300px] md:w-full md:px-6 flex flex-col'>
+        <label className='text-sm text-white md:text-lg'> Mensaje </label>
           <textarea
+          className='w-full min-h-[150px] border-2  rounded-md text-start '
           placeholder="Mensaje..."
           name="message"
           onChange={handleChange}
           />
        </div>
+
+       <div className='flex justify-center items-center'><button type="submit" className='bg-[#bdbdbd] px-4 py-1 my-2 md:px-6 rounded-lg'>Enviar</button></div>
 
     </form>
   )
