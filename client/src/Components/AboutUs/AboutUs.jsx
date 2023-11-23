2fb4aeeb3b277bfb30d08aa5689fa1def906fa90
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-23 17:37:32 -0300

    about cover

--- client/src/Components/AboutUs/AboutUs.jsx
+++ client/src/Components/AboutUs/AboutUs.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import logo from '../../../public/logoWhite.png'
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <>
+    <div className='bgImage2 h-80 w-full md:h-[280px] object-scale-down mt-20 flex justify-center items-center'>
+     <img src={logo} className='h-60 w-60'/>
+
+    </div>
+    </>
   )
 }
 
