17e8f24dad7c50c23e6cdfbe889a32fa7b90192f
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-24 14:24:58 -0300

    paginate render works

--- client/src/Components/Paginate/Paginate.jsx
+++ client/src/Components/Paginate/Paginate.jsx
@@ -1,13 +1,40 @@
 import React, {useEffect} from 'react'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { paginado } from '../../Redux/actions'
 import { MdKeyboardDoubleArrowRight, MdKeyboardDoubleArrowLeft, MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 
 function Paginate() {
-
     
+  const dispatch = useDispatch()
+  const pages = useSelector(state => state.pages)
+  const currentPage = useSelector(state => state.currentPage)
+
+  const handleValue =(value)=>{
+    dispatch(paginado(value)) 
+  }
+
   return (
-    <div>Paginate</div>
+    <div>
+      <div>
+        <button onClick={()=> handleValue("start")}><MdKeyboardDoubleArrowLeft size={20}/></button>
+        <button onClick={()=> handleValue("prev")}><MdKeyboardArrowLeft size={20}/></button>
+
+        {
+          pages.map((n)=>{
+            return ( 
+              <button 
+                 style={{color: currentPage === n? "red": "black"}}
+                 onClick={()=> handleValue(n)}>{n}
+              </button> 
+            )
+          })
+        }
+
+        <button onClick={()=> handleValue("next")}><MdKeyboardArrowRight size={20}/></button>
+        <button onClick={()=> handleValue("end")}><MdKeyboardDoubleArrowRight size={20}/></button>
+
+      </div>
+    </div>
   )
 }
 
