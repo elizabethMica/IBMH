b7439c730ca502c0eb00b9775c7b0171e57378d6
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-26 20:08:08 -0300

    div contact

--- client/src/Components/Landing/InfoMeeting.jsx
+++ client/src/Components/Landing/InfoMeeting.jsx
@@ -6,6 +6,7 @@ import { MdOpenInNew } from "react-icons/md";
 import {MdEmail} from 'react-icons/md'
 import {BsSpotify} from 'react-icons/bs'
 import {FaFacebookSquare} from 'react-icons/fa'
+import cruzPaisaje from '../../assets/pictures/cruz-cristiana-paisaje-natural.jpg'
 
 
 function InfoMeeting() {
@@ -22,15 +23,15 @@ function InfoMeeting() {
     <section className=' my-8 md:my-12 py-16  flex justify-center items-center'>
       <div className='flex flex-col justify-center bgImage h-80 w-full md:h-[400px] object-center object-none'>
 
-      <div className='flex flex-col md:flex-row px-8 md:gap-20 md:justify-around'>
+      <div className='flex flex-col md:flex-row px-8 md:gap-1 md:justify-center'>
 
-        <div className='flex flex-col justify-center items-center '>
+        <div className='flex flex-col justify-center items-center md:mx-8'>
           <FaChurch size={30} color={"white"} className='mb-2 md:mb-0' />
           {/* <h3 className='text-lg font-bold text-center'>Reunión general:</h3> */}
           <h3 className='text-xl font-semibold text-center text-white'>Domingo 10:30hs</h3>
         </div>
 
-        <div className='flex flex-col justify-start items-center mt-6 md:mt-0'>
+        <div className='flex flex-col justify-start items-center mt-6 md:mt-0 md:mx-8'>
           <FaLocationDot size={30} color={"white"} className='mb-2 md:mb-0'/>
           <Link to={mapsLink} target="_blank">
             {/* <h3 className='text-lg font-bold text-center'>Ubicación:</h3> */}
@@ -41,7 +42,7 @@ function InfoMeeting() {
           </Link>
         </div>
 
-        <div className='flex flex-col justify-start items-center mt-6 md:mt-0'>
+        <div className='flex flex-col justify-start items-center mt-6 md:mt-0 md:mx-8'>
            <MdEmail size={30} color={"white"} className='mt-2 md:mt-0' />
          <a href="mailto:ibmhurlingham@gmail.com">
            <h3 className='text-xl font-semibold text-center text-white'>ibmhurlingham@gmail.com</h3>
