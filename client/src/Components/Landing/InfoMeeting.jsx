e41bacc9e93052235c9437ba0718a2dd9c73f6d4
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-26 21:16:07 -0300

    contact info render

--- client/src/Components/Landing/InfoMeeting.jsx
+++ client/src/Components/Landing/InfoMeeting.jsx
@@ -26,7 +26,7 @@ function InfoMeeting() {
       <div className='flex flex-col md:flex-row px-8 md:gap-1 md:justify-center'>
 
         <div className='flex flex-col justify-center items-center md:mx-8'>
-          <FaChurch size={30} color={"black"} className='mb-2 md:mb-0' />
+          <FaChurch size={30} color={"black"} className='mb-2 md:mb-0 w-24' />
           {/* <h3 className='text-lg font-bold text-center'>Reunión general:</h3> */}
           <h3 className='text-xl font-semibold text-center text-black'>Domingo 10:30hs</h3>
         </div>
@@ -49,14 +49,14 @@ function InfoMeeting() {
           </a>
         </div>
 
+        </div>
         <div>
-          <h3 className='font-semibold text-xl m-8 text-center md:text-3xl'>También puedes acceder al contenido de la iglesia en:</h3>
+          <h3 className='font-semibold text-xl m-8 pt-8 text-center md:text-2xl'>También puedes acceder al contenido de la iglesia en:</h3>
           <div className='flex flex-row justify-center items-center'>
             <Link to={spotifyLink} target="_blank"><BsSpotify size={40} className='mx-4' color={"#00e461"}/></Link>
             <Link to={youtubeLink} target="_blank"><FaYoutube size={40} className='mx-4' color={"bb0000"}/></Link>
             <Link to={facebookLink} target="_blank"><FaFacebookSquare size={40} className='mx-4' color={"#3b5998"}/></Link>
           </div>
-        </div>
       </div>
 
     </div>
