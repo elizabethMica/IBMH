fcb18fd7fbfd5bf87943b08ddb6bda8b35a30ee2
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-26 21:12:51 -0300

    contact

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -15,26 +15,29 @@ function Contact() {
   },[])
   return (
     <div className='mt-20'>
-        <InfoMeeting/>
       <div>
-        {/* <img src={mapa} alt="Schumann 2334, Hurlingham"/> */}
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.604622047778!2d-58.64995482427317!3d-34.58886955671411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcbfb4a6a7a6dd%3A0x563ff2a2cb47863f!2sIglesia%20Bautista%20Misionera%20de%20Hurlingham!5e0!3m2!1ses-419!2sar!4v1701041540838!5m2!1ses-419!2sar" 
+       
+      </div>
+      <div>
+      <h3 className='font-semibold text-lg m-8 text-center md:text-3xl'>Quieres contactarte con nosotros? Llena el formulario con tus dudas!</h3>
+     
+      <FormContact/>
+      </div>
+      {/* <div>
+          <h3 className='font-semibold text-lg m-8 text-center md:text-3xl'>También puedes acceder al contenido de la iglesia en:</h3>
+          <div className='flex flex-row justify-center items-center'>
+            <Link to={spotifyLink} target="_blank"><BsSpotify size={40} className='mx-4' color={"#00e461"}/></Link>
+            <Link to={youtubeLink} target="_blank"><FaYoutube size={40} className='mx-4' color={"bb0000"}/></Link>
+          </div>
+        </div> */}
+        <InfoMeeting/>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.604622047778!2d-58.64995482427317!3d-34.58886955671411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcbfb4a6a7a6dd%3A0x563ff2a2cb47863f!2sIglesia%20Bautista%20Misionera%20de%20Hurlingham!5e0!3m2!1ses-419!2sar!4v1701041540838!5m2!1ses-419!2sar" 
        
         className="border-none w-full h-96" 
         allowfullscreen="" 
         loading="lazy" 
         referrerpolicy="no-referrer-when-downgrade">
         </iframe>
-
-        <div>
-          <h3 className='font-semibold text-lg m-8 text-center'>También puedes acceder al contenido de la iglesia en:</h3>
-          <div className='flex flex-row justify-center items-center'>
-            <BsSpotify size={30} className='mx-4' color={"#00e461"}/>
-            <FaYoutube size={30} className='mx-4' color={"bb0000"}/>
-          </div>
-        </div>
-      </div>
-      <FormContact/>
     </div>
   )
 }
--- client/src/Components/Landing/InfoMeeting.jsx
+++ client/src/Components/Landing/InfoMeeting.jsx
@@ -1,48 +1,61 @@
 import React from 'react'
 import {Link, NavLink} from 'react-router-dom'
 import {BiChurch} from 'react-icons/bi'
-import {FaLocationDot, FaChurch} from 'react-icons/fa6'
+import {FaLocationDot, FaChurch,FaYoutube} from 'react-icons/fa6'
 import { MdOpenInNew } from "react-icons/md";
 import {MdEmail} from 'react-icons/md'
 import {FaFacebookSquare} from 'react-icons/fa'
+import {BsSpotify} from 'react-icons/bs'
 
 
 function InfoMeeting() {
 
   const mapsLink = "https://maps.app.goo.gl/ofYdrW9GDmyXzmsp6"
   const facebookLink = 'https://facebook.com/ibmhurlingham/'
+  const youtubeLink = 'https://www.youtube.com/@ibmhurlingham879'
+  const spotifyLink = 'https://open.spotify.com/show/2u3WpNOhXByNh9ifTLlgCQ?si=9452c98f2180480e'
+
 
 
   return (
  
 
     <section className='  mt-8 flex justify-center items-center'>
-      <div className='flex flex-col justify-center bgImage h-80 w-full md:h-[400px] object-center object-none'>
+      <div className='flex flex-col justify-center bgImage bg-gray-300 h-full w-full md:h-[400px] py-16 object-center object-none'>
 
       <div className='flex flex-col md:flex-row px-8 md:gap-1 md:justify-center'>
 
         <div className='flex flex-col justify-center items-center md:mx-8'>
-          <FaChurch size={30} color={"white"} className='mb-2 md:mb-0' />
+          <FaChurch size={30} color={"black"} className='mb-2 md:mb-0' />
           {/* <h3 className='text-lg font-bold text-center'>Reunión general:</h3> */}
-          <h3 className='text-xl font-semibold text-center text-white'>Domingo 10:30hs</h3>
+          <h3 className='text-xl font-semibold text-center text-black'>Domingo 10:30hs</h3>
         </div>
 
         <div className='flex flex-col justify-start items-center mt-6 md:mt-0 md:mx-8'>
-          <FaLocationDot size={30} color={"white"} className='mb-2 md:mb-0'/>
+          <FaLocationDot size={30} color={"black"} className='mb-2 md:mb-0'/>
           <Link to={mapsLink} target="_blank">
             {/* <h3 className='text-lg font-bold text-center'>Ubicación:</h3> */}
-            <h3 className='text-xl font-semibold text-center text-white'>Schumann 2334, Hurlingham, Buenos Aires</h3>
+            <h3 className='text-xl font-semibold text-center text-black'>Schumann 2334, Hurlingham, Buenos Aires</h3>
           </Link>
           <Link to={mapsLink} target="_blank">
-            <MdOpenInNew size={15} color={"white"}/>
+            <MdOpenInNew size={15} color={"black"}/>
           </Link>
         </div>
 
         <div className='flex flex-col justify-start items-center mt-6 md:mt-0 md:mx-8'>
-           <MdEmail size={30} color={"white"} className='mt-2 md:mt-0' />
+           <MdEmail size={30} color={"black"} className='mt-2 md:mt-0' />
          <a href="mailto:ibmhurlingham@gmail.com">
-           <h3 className='text-xl font-semibold text-center text-white'>ibmhurlingham@gmail.com</h3>
+           <h3 className='text-xl font-semibold text-center text-black'>ibmhurlingham@gmail.com</h3>
           </a>
+        </div>
+
+        <div>
+          <h3 className='font-semibold text-xl m-8 text-center md:text-3xl'>También puedes acceder al contenido de la iglesia en:</h3>
+          <div className='flex flex-row justify-center items-center'>
+            <Link to={spotifyLink} target="_blank"><BsSpotify size={40} className='mx-4' color={"#00e461"}/></Link>
+            <Link to={youtubeLink} target="_blank"><FaYoutube size={40} className='mx-4' color={"bb0000"}/></Link>
+            <Link to={facebookLink} target="_blank"><FaFacebookSquare size={40} className='mx-4' color={"#3b5998"}/></Link>
+          </div>
         </div>
       </div>
 
