3059a4290146bf8e08a968380420672f2def11c4
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-11-27 12:28:31 -0300

    reload filters done

--- client/src/Components/Filters/Filters.jsx
+++ client/src/Components/Filters/Filters.jsx
@@ -10,11 +10,17 @@ import {filterBook, getAllSermon} from '../../Redux/actions'
 function Filters() {
 
     const dispatch = useDispatch()
-    const [book, setBook] = useState("")
+    const [book, setBook] = useState([])
 
     const handleBook = (event)=>{
      event.preventDefault()
+     setBook([event.target.value])
      dispatch(filterBook(event.target.value))
+    }
+
+    const handleDispatch =()=>{
+        dispatch(getAllSermon())
+        setBook("")
     }
 
   return (
@@ -32,6 +38,12 @@ function Filters() {
                     })}
 
         </select>
+        {
+            book?.length>0 ? (
+                <button onClick={handleDispatch} className='hover:bg-red-500 hover:text-white px-2 py-1 rounded-md text-xs bg-gray-300 mx-2'>Borrar filtro X</button>
+            ) : null
+            
+        }
     </div>
     
     )
