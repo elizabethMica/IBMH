5384b85723c90bb5c76b8ea79c3e974d615b9c66
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-04 14:18:45 -0300

    sermon cover

--- client/src/Components/Sermon/SermonCard.jsx
+++ client/src/Components/Sermon/SermonCard.jsx
@@ -4,12 +4,16 @@ import { NavLink, Link } from "react-router-dom"
 
 const SermonCard = (Props) => {
 
-  
+  const embed = "https://www.youtube.com/embed/"
+  const videoLink = Props?.videoYT?.split("/")[3]
+  const videoID = videoLink.length[-1]
+
+  const thumbURL = 'https://img.youtube.com/vi/'+ videoID + '/0.jpg'
 
   return (
     <div className=" my-6 md:my-0 w-72 md:w-64" >
       <Link to={`/sermon/${Props.id}`} >
-        <img  src={Props.cover} alt={`Imagen del sermon ${Props.title}`} className="rounded-lg w-[300px] h-[200px] md:w-[400px] md:h-[180px]" />
+        <img  src={Props?.cover} alt={`Imagen del sermon ${Props.title}`} className="rounded-lg w-[300px] h-[200px] md:w-[400px] md:h-[180px]" />
       </Link>
       <div className="pt-2"  >
         <p className="text-sm" >{Props.date}</p>
