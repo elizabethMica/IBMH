bfdaa4d90cc19dd73aac9a4de5570c5f47b72bb4
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-05 16:00:34 -0300

    background pic contact

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -15,16 +15,16 @@ function Contact() {
     window.scrollTo(0, 0)
   },[])
   return (
-    <div className='mt-20'>
+    <div className='mt-20 '>
+      <div className='bgImage3'>
       <div className='flex flex-col md:flex-row md:justify-between justify-center md:items-start items-center bg-[#be845200] md:px-14'>
-      <div>
-        <h3 className='font-semibold text-2xl mx-8 pt-8 text-center md:text-4xl text-black'>Contáctate con nosotros!</h3>
-        <p className='text-lg px-8 py-4 md:text-xl text-justify md:text-start w-[400px] text-black '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam vel, quis repellat unde corporis pariatur itaque commodi, odio dolor eius corrupti nulla quo ullam, facilis quia ipsa suscipit error est.</p>
-        
+        <div>
+          <h3 className='font-semibold text-2xl mx-8 pt-8 text-center md:text-4xl text-black'>Contáctate con nosotros!</h3>
+          <p className='text-lg px-8 py-4 md:text-xl text-justify md:text-start w-[400px] text-black '>Si tienes alguna duda acerca del evangelio o nuestras reuniones, alguna pregunta o simplemente quieres mandar un mensaje. No dudes en hacerlo y te contactaremos a la brevedad!</p>
+        </div>
+       <FormContact/>
       </div>
-      <FormContact/>
-      </div>
-      
+      </div> 
       <InfoMeeting/>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.604622047778!2d-58.64995482427317!3d-34.58886955671411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcbfb4a6a7a6dd%3A0x563ff2a2cb47863f!2sIglesia%20Bautista%20Misionera%20de%20Hurlingham!5e0!3m2!1ses-419!2sar!4v1701041540838!5m2!1ses-419!2sar" 
        
@@ -33,6 +33,7 @@ function Contact() {
         loading="lazy" 
         referrerpolicy="no-referrer-when-downgrade">
         </iframe>
+       
     </div>
   )
 }
