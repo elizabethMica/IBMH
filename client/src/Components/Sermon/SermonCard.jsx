7fe81e6bbfd4f8d02934e99c1b61c5d32e691201
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-07 12:45:18 -0300

    img object-none

--- client/src/Components/Sermon/SermonCard.jsx
+++ client/src/Components/Sermon/SermonCard.jsx
@@ -13,7 +13,7 @@ const SermonCard = (Props) => {
   return (
     <div className=" my-6 md:my-0 w-72 md:w-64" >
       <Link to={`/sermon/${Props.id}`} >
-        <img  src={Props?.cover} alt={`Imagen del sermon ${Props.title}`} className="rounded-lg w-[300px] h-[200px] md:w-[400px] md:h-[180px]" />
+        <img  src={Props?.cover} alt={`Imagen del sermon ${Props.title}`} className="object-none object-center rounded-lg w-[300px] h-[200px] md:w-[400px] md:h-[180px]" />
       </Link>
       <div className="pt-2"  >
         <p className="text-sm" >{Props.date}</p>
