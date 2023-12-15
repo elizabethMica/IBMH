c65a7157a6b82f9d313b24e020b52e9ad0878887
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-15 20:23:29 -0300

    code cleaning landing

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -24,10 +24,6 @@ const Landing = () => {
 
   return (
     <>
-      
-      {/* <div className='bgImage2 h-80 w-full md:h-[280px] object-scale-down mt-20 flex justify-center items-center'>
-     <img src={logo} className='h-60 w-60'/>
-    </div> */}
       <div className='pt-24 md:pt-20 '>
 
       <section >
