0c5d7133d7c41fe28fc853e63df41f59c250cab3
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2023-12-15 20:17:36 -0300

    info style

--- client/src/Components/Landing/InfoMeeting.jsx
+++ client/src/Components/Landing/InfoMeeting.jsx
@@ -19,34 +19,34 @@ function InfoMeeting() {
  
 
     <section className=' flex justify-center items-center '>
-      <div className='flex flex-col justify-center bgImage bg-gradient-to-t from-orange-200 to-white h-full w-full md:h-[400px] py-16 object-center object-none'>
+      <div className='flex flex-col md:flex-row justify-center bgImage bg-gradient-to-t from-orange-200 to-white h-full w-full md:h-[400px] py-16 object-center object-none'>
 
-      <div className='flex flex-col md:flex-row px-8 md:gap-1 md:justify-center'>
+      <div className='flex flex-col  px-8 md:gap-10 md:justify-center md:items-start'>
 
-        <div className='flex flex-col justify-center items-center md:mx-8'>
-          <FaChurch size={30} color={"black"} className='mb-2 md:mb-0 w-24' />
+        <div className='flex flex-col md:flex-row justify-center items-center md:items-start md:mx-8'>
+          <FaChurch size={30} color={"black"} className='mb-2 md:mb-0 md:mx-2' />
           <h3 className='text-xl font-semibold text-center text-black'>Domingo 10:30hs</h3>
         </div>
 
-        <div className='flex flex-col justify-start items-center mt-6 md:mt-0 md:mx-8'>
-          <FaLocationDot size={30} color={"black"} className='mb-2 md:mb-0'/>
+        <div className='flex flex-col md:flex-row justify-start items-center md:items-start mt-6 md:mt-0 md:mx-8'>
+          <FaLocationDot size={30} color={"black"} className='mb-2 md:mb-0 md:mx-2'/>
           <Link to={mapsLink} target="_blank">
-            <h3 className='text-xl font-semibold text-center text-black'>Schumann 2334, Hurlingham, Buenos Aires</h3>
+            <h3 className='text-xl font-semibold text-center text-black'>Schumann 2334, Hurlingham</h3>
           </Link>
          
         </div>
 
-        <div className='flex flex-col justify-start items-center mt-6 md:mt-0 md:mx-8'>
-           <MdEmail size={30} color={"black"} className='mt-2 md:mt-0' />
+        <div className='flex flex-col md:flex-row justify-start items-center md:items-start mt-6 md:mt-0 md:mx-8'>
+           <MdEmail size={30} color={"black"} className='mt-2 md:mt-0 md:mx-2' />
          <a href="mailto:ibmhurlingham@gmail.com">
            <h3 className='text-xl font-semibold text-center text-black'>ibmhurlingham@gmail.com</h3>
           </a>
         </div>
 
         </div>
-        <div>
-          <h3 className='font-semibold text-xl m-8 pt-8 text-center md:text-2xl'>También puedes acceder al contenido de la iglesia en:</h3>
-          <div className='flex flex-row justify-center items-center'>
+        <div className='justify-center flex flex-col items-center md:border-l-2 md:border-white md:px-8 '>
+          <h3 className='font-semibold text-xl md:mx-6 md:m-0 m-8 py-8 md:pt-0 text-center '>También puedes acceder al contenido de la iglesia en</h3>
+          <div className='flex flex-row  justify-center items-center'>
             <Link to={spotifyLink} target="_blank"><BsSpotify size={40} className='mx-4' color={"#00e461"}/></Link>
             <Link to={youtubeLink} target="_blank"><FaYoutube size={40} className='mx-4' color={"bb0000"}/></Link>
             <Link to={facebookLink} target="_blank"><FaFacebookSquare size={40} className='mx-4' color={"#3b5998"}/></Link>
