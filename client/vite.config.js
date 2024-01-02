785978592a16d68981bd96bc95aa475f5b95bd15
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2024-01-02 16:28:06 -0300

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Directorio de salida para los archivos compilados
+    minify: true,   // Minificar el código en producción
+    sourcemap: false, // Deshabilitar los mapas de origen en producción (mejora el rendimiento)
+  },
+  server: {
+    port: process.env.PORT || 5173
+  }
 })
