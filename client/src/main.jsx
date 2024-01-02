7e788d6ccdac404fea44101b83ce0ccc2faf4c58
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2024-01-01 22:48:28 -0300

    react strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,11 @@ import {Provider} from 'react-redux'
 import {store} from './Redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
+  </Provider>
+  </React.StrictMode>
 );
