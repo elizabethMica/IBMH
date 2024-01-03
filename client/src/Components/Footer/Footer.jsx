ef016c380d645e29a83adb9c8b45c8977ad1f8fa
elizabethMica <121364740+elizabethMica@users.noreply.github.com>
2024-01-03 17:51:29 -0300

    footer style mobile view done

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -25,17 +25,10 @@ function Footer() {
               <h3 className='text-xl  text-white'>Iglesia Bautista Misionera</h3>
               <p className='text-sm  text-white'>Pastor Diego Velazquez</p>
             </div>
-        {/* <div className='bg-[#443426] flex flex-col justify-center items-center text-center px-4 md:w-72 md:text-start'>
-            <div className='bg-[#443426]'>
-              <h3 className='text-lg  text-white'>Iglesia Bautista Misionera</h3>
-               <p className='text-white text-center md:text-start px-4 md:px-0 pt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ex debitis, sunt doloribus dolore iste natus laborum id, hic, ullam odit minima numquam quod recusandae tempore ipsum quis architecto provident?</p>
-            <h5 className='text-sm  text-white md:py-4  py-2'><NavLink to="/about" className={"hover:text-[#bd8351]"}>Conoce más acerca de la iglesia ACÁ</NavLink></h5> 
-            </div>
-          </div> */}
 
         <div className='bg-[#443426] flex  flex-col justify-center md:items-start text-center my-4 md:my-0 md:text-start'>
           <h3 className='text-lg  text-white md:pb-4  pb-2'><NavLink to="/contact">Contacto</NavLink></h3> {/* navlink a contacto */}
-          <div className='bg-[#443426] flex flex-col mx-4 text-white md:items-start md:m-0'>
+          <div className='bg-[#443426] flex flex-col md:mx-4 text-white md:items-start md:m-0'>
               <div className=' flex flex-row justify-center text-center md:pb-4 md:text-start pb-2'>
                 <FaChurch color={'white'} size={20} className='mx-2'/> Reunión general: Domingo 10:30hs
               </div>
@@ -51,10 +44,10 @@ function Footer() {
       
         <div className='bg-[#443426] flex  flex-col justify-center text-center my-4 md:my-0 md:text-start'>
           <h3 className='text-lg  text-white md:pb-4 pb-2'>Redes Sociales</h3>
-          <div className='bg-[#443426] flex flex-col mx-4 text-white justify-center items-center text-center md:items-start md:m-0'>
+          <div className='bg-[#443426] flex flex-col md:mx-4 text-white justify-center items-center text-center md:items-start md:m-0'>
               <div className='flex flex-row justify-center items-center md:pb-4 md:text-start text-center pb-2'>
                 <FaYoutube color={'white'} size={20} className='mx-2'/>
-                <NavLink to={youtubeLink} target="_blank"> IBM Hurlingham Iglesia Bautista</NavLink> 
+                <NavLink to={youtubeLink} target="_blank"> IBM Hurlingham</NavLink> 
                 <MdOpenInNew size={10} className='ml-1'/>
               </div>
               <div className='flex flex-row justify-center items-center md:pb-4 md:text-start text-center pb-2' >
@@ -64,11 +57,11 @@ function Footer() {
               </div>
               <div className='flex flex-row justify-center items-center md:pb-4 md:text-start text-center pb-2' >
                 <FaFacebookSquare color={'white'} size={20} className='mx-2'/> 
-                <NavLink to={facebookLink} target="_blank">Iglesia Bautista Misionera Hurlingham </NavLink>
+                <NavLink to={facebookLink} target="_blank">IBM Hurlingham </NavLink>
                 <MdOpenInNew size={10} className='ml-1'/>
               </div>
           </div>
-          <NavLink to="/contact" className={"lg:mt-16 mt-4 lg:self-end self-center"}><button className='text-lg bg-[#be8552] hover:bg-[#9c6d44] rounded-lg font-semibold  text-white p-2'>Contáctanos</button></NavLink>
+          <NavLink to="/contact" className={"lg:mt-16 mt-4 lg:self-end self-center"}><button className='text-lg bg-[#be8552] hover:bg-[#9c6d44] rounded-md font-semibold  text-white p-2'>CONTÁCTANOS</button></NavLink>
         </div>
       </div>
       <div className='bg-[#443426] justify-center items-center flex pb-4 border-none'>
